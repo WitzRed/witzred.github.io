1da1ff43d8d3b8afa2e00b7b5f0f53cea683b828
Witenberg S R Souza <wittenberg.rs@gmail.com>
2022-11-11 12:51:38 -0500

    cropped photos and added filter selection

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ async function FecthRepo(){
                             files.forEach(file =>{
                                 let divEl = document.createElement('div');
                                 divEl.id = file.name.split('.')[0];
-                                divEl.className = "panel";
+                                divEl.className = "panel" + " " + subdir.name;
                                 divEl.style.backgroundImage = "url("+file.download_url+")";
                                 divEl.style.backgroundSize= "cover";
                                 divEl.style.backgroundRepeat= "no-repeat";
@@ -52,6 +52,8 @@ async function FecthRepo(){
                             let footEl = document.createElement('footer');
                             footEl.className = "footer";
                             portfolio.appendChild(footEl);
+                            //add filter Selection function;
+                            filterSelection('all'); 
                         }
                     })
                    
@@ -61,5 +63,59 @@ async function FecthRepo(){
 }
 
 FecthRepo();
+// filterSelection('all');
+function filterSelection(c){
 
-function filterSelection(){}
+    let element, i;
+    element = document.getElementsByClassName("panel");
+
+    if(c == "all"){
+        c = "";
+    }
+
+    //Add "show" class (display:block) to filtered elements, and remove "show" class from elements that are not selsected
+   
+    for(i = 0; i < element.length; i++){
+        removeClass(element[i], "show");
+        if (element[i].className.indexOf(c) > -1){
+            addClass(element[i], "show");
+        }
+    }
+}
+
+//Show filtered Elements
+function addClass(element, name){
+    let i, currentClasses, classesToInsert;
+    currentClasses = element.className.split(" ");
+    classesToInsert = name.split(" ");
+    for(i = 0; i< classesToInsert.length; i++){
+        if(currentClasses.indexOf(classesToInsert[i]) == -1){
+            element.className += " " + classesToInsert[i];
+        }
+    }
+
+}
+
+//Hide lements that are not selected
+function removeClass(element, name){
+    let i, currentClasses, classesToRemove;
+    currentClasses = element.className.split(" ");
+    classesToRemove = name.split(" ");
+    for(i=0; i < classesToRemove.length; i++){
+        while (currentClasses.indexOf(classesToRemove[i]) > -1){
+            currentClasses.splice(currentClasses.indexOf(classesToRemove[i]), 1);
+        }
+    }
+    element.className = currentClasses.join(" ");
+}
+
+//Add active class to the current control button (highlight it)
+let btnContaniner = document.getElementById("portfolioFilter");
+let btns = btnContaniner.getElementsByClassName("btn");
+for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener("click", function(){
+        let currentBtn = document.getElementsByClassName("active");
+        currentBtn[0].className = currentBtn[0].className.replace("active", "");
+        this.className += " active";
+    });
+}
